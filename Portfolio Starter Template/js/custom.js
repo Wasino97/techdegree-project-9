350e03631905243745490b1edc8e060b476ae5d3
Wasino97 <salazarjaycee@gmail.com>
2022-04-06 16:31:50 -0500

    finished portfolio

--- Portfolio Starter Template/js/custom.js
+++ Portfolio Starter Template/js/custom.js
@@ -32,37 +32,6 @@ $(document).ready(function () {
     }
   });
 
-  // show the portfolio modal on portfolio item click
-  $(".portfolio-item").on("click", function () {
-    var image = $(this).find("img");
-
-    // remove all elements from portfolio modal
-    $(".portfolio-modal .image-wrapper").empty();
-    // add relevant image
-    $(".portfolio-modal .image-wrapper").append(
-      `<img src="${image.attr("src")}"/>`
-    );
-    // show the modal
-    $(".portfolio-modal").fadeIn(300);
-    $("body").addClass("no-scroll");
-  });
-
-  // close modal on click anywhere but the image
-  $(".portfolio-modal").on("click", function (e) {
-    if (!$(e.target).is("img")) {
-      $(".portfolio-modal").fadeOut(300);
-      $("body").removeClass("no-scroll");
-    }
-  });
-
-  // close modal on esc click
-  $(document).on("keydown", function (event) {
-    if (event.key == "Escape") {
-      $(".portfolio-modal").fadeOut(300);
-      $("body").removeClass("no-scroll");
-    }
-  });
-
   // add active class during scroll to nav-links
   $(window)
     .scroll(function () {
